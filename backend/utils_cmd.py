ccec3c8f5c0a9a57e1818b9b30e23ddb7ae9818d
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-04-21 19:39:02 +0300

    Added cli util for app data management. Current functional: * add user * base interactive functions

---
+++ backend/utils_cmd.py
@@ -0,0 +1,39 @@
+import os
+from utils.add_user import add_user
+
+
+def help_dialog():
+    text = """
+    Available commands:
+ exit - exit the program
+ help - show this message    
+ add_user - add a new user
+    
+    """
+    print(text)
+
+def main():
+    print("Welcome to debug console. use 'help' to see available commands.")
+    while True:
+        cmd = input("> ")
+        match cmd:
+            case "exit":
+                break
+            case "help":
+                help_dialog()
+            case "add_user":
+                add_user()
+            case _:
+                print("Unknown command.")
+    print()
+    print("Goodbye!")
+
+
+if __name__ == '__main__':
+    from db import global_init
+    if os.environ.get("DOTENV", False):
+        from dotenv import load_dotenv
+        load_dotenv()
+
+    global_init()
+    main()
