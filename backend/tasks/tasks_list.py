2dce2b6768343fde6de3af6b021c9a6f68968e07
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-04-28 23:38:09 +0300

    Fixed lots of bugs in tasks_list.py. Added more data to responses.

--- backend/tasks/tasks_list.py
+++ backend/tasks/tasks_list.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
-from ORM.tasks import Task, TaskShareLevel, TaskStatus
+from ORM.tasks import Task, TaskShareLevel, TaskStatus, WRITABLE_POLITICS, READABLE_POLITICS
 from ORM.users import User
 
 def get_user_tasks(
@@ -34,8 +34,15 @@ Function to search user's tasks
             return 403
         filter_user =  session.query(User).filter_by(id=filter_user).first()
 
+    q1 = session.query(Task).filter(Task.owner_id == filter_user.id)
+    if write_permission_required:
+        # q2 = session.query(Task).filter(Task.access_politics <= TaskShareLevel.RW_ONLY_1_LEVELS)
+        q2 = session.query(Task).filter(Task.access_politics.in_(WRITABLE_POLITICS))
+    else:
+        # q2 = session.query(Task).filter((Task.access_politics <= TaskShareLevel.RW_ONLY_1_LEVELS) | (Task.access_politics >= TaskShareLevel.R_ONLY_1_LEVELS))
+        q2 = session.query(Task).filter(Task.access_politics.notin_([TaskShareLevel.PARENT_SELECT, TaskShareLevel.PRIVATE]))
 
-    query = session.query(Task).filter(Task.owner_id == filter_user)
+    query = q1.union(q2)
 
     if filter_status:
         query = query.filter(Task.status == TaskStatus[filter_status])
@@ -48,27 +55,38 @@ Function to search user's tasks
             )
         )
 
-    if write_permission_required:
-        query = query.filter(Task.access_politics.in_([
-            TaskShareLevel.RW_ALL,
-            TaskShareLevel.RW_ONLY_1_LEVELS,
-            TaskShareLevel.RW_ONLY_2_LEVELS
-        ]))
-    else:
-        query = query.filter(Task.access_politics.in_([
-            TaskShareLevel.R_ALL,
-            TaskShareLevel.R_ONLY_1_LEVELS,
-            TaskShareLevel.R_ONLY_2_LEVELS,
-            # TaskShareLevel.PARENT_SELECT
-        ]))
+    # if write_permission_required:
+    #     query = query.filter(Task.access_politics.in_([
+    #         TaskShareLevel.RW_ALL,
+    #         TaskShareLevel.RW_ONLY_1_LEVELS,
+    #         TaskShareLevel.RW_ONLY_2_LEVELS
+    #     ]))
+    # else:
+    #     query = query.filter(Task.access_politics.in_([
+    #         TaskShareLevel.R_ALL,
+    #         TaskShareLevel.R_ONLY_1_LEVELS,
+    #         TaskShareLevel.R_ONLY_2_LEVELS,
+    #         # TaskShareLevel.PARENT_SELECT
+    #     ]))
+
 
     if short_response:
-        query = query.with_entities(Task.id, Task.title, Task.status)
+        query = query.with_entities(Task.id, Task.title, Task.status, Task.parent)
     else:
-        query = query.with_entities(Task.id, Task.title, Task.description, Task.status, Task.creation_date, Task.deadline)
+        query = query.with_entities(
+            Task.id,
+            Task.title,
+            Task.parent,
+            Task.description,
+            Task.status,
+            Task.creation_date,
+            Task.deadline,
+            Task.access_politics,
+            Task.owner_id
+        )
 
-    if filter_user is not None:
-        query = query.filter(Task.owner_id == filter_user)
+    # if filter_user is not None:
+    #     query = query.filter(Task.owner_id == filter_user.id)
 
     tasks = query.all()
 
@@ -77,13 +95,16 @@ Function to search user's tasks
         task_dict = {
             "id": task.id,
             "title": task.title,
-            "status": task.status.name
+            "status": task.status.name,
+            "parent": task.parent,
         }
         if not short_response:
             task_dict.update({
                 "description": task.description,
                 "creation_date": task.creation_date,
-                "deadline": task.deadline
+                "deadline": task.deadline,
+                "parent": task.parent,
+                "writable": task.access_politics in WRITABLE_POLITICS or user.id == task.owner_id,
             })
         result.append(task_dict)
 
