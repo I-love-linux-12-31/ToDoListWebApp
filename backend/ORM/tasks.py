b3cd995b58479bd37dd38463d2312a15911745b8
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-04-28 23:31:26 +0300

    Updated models

--- backend/ORM/tasks.py
+++ backend/ORM/tasks.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from tkinter import READABLE
+
 from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, DateTime, Enum, VARCHAR
 
 import enum
@@ -24,6 +26,81 @@ class TaskShareLevel(enum.Enum):
     RW_ALL = -127
     RW_ONLY_1_LEVELS = -1
     RW_ONLY_2_LEVELS = -2
+
+    @staticmethod
+    def level2int(level):
+        match level:
+            case TaskShareLevel.PARENT_SELECT:
+                return 0
+            case TaskShareLevel.PRIVATE:
+                return 128
+            case TaskShareLevel.R_ALL:
+                return 127
+            case TaskShareLevel.R_ONLY_1_LEVELS:
+                return 1
+            case TaskShareLevel.R_ONLY_2_LEVELS:
+                return 2
+            case TaskShareLevel.RW_ALL:
+                return -127
+            case TaskShareLevel.RW_ONLY_1_LEVELS:
+                return -1
+            case TaskShareLevel.RW_ONLY_2_LEVELS:
+                return 2
+            case _:
+                return 0
+
+    @staticmethod
+    def int2level(level):
+        match level:
+            case 0:
+                return TaskShareLevel.PARENT_SELECT
+            case 128:
+                return TaskShareLevel.PRIVATE
+            case 127:
+                return TaskShareLevel.R_ALL
+            case 1:
+                return TaskShareLevel.R_ONLY_1_LEVELS
+            case 2:
+                return TaskShareLevel.R_ONLY_2_LEVELS
+            case -127:
+                return TaskShareLevel.RW_ALL
+            case -1:
+                return TaskShareLevel.RW_ONLY_1_LEVELS
+            case -2:
+                return TaskShareLevel.RW_ONLY_2_LEVELS
+            case _:
+                return 0
+
+    # def __gt__(self, other):
+    #     return TaskShareLevel.level2int(self) > TaskShareLevel.level2int(other)
+    #
+    # def __lt__(self, other):
+    #     return TaskShareLevel.level2int(self) < TaskShareLevel.level2int(other)
+    #
+    # def __gte__(self, other):
+    #     return TaskShareLevel.level2int(self) >= TaskShareLevel.level2int(other)
+    #
+    # def __lte__(self, other):
+    #     return TaskShareLevel.level2int(self) <= TaskShareLevel.level2int(other)
+
+    # @staticmethod
+    # def is_in(instance, iterable):
+    #     for item in iterable:
+    #         if isinstance(item, TaskShareLevel) and TaskShareLevel.level2int(item) == TaskShareLevel.level2int(instance):
+    #             return True
+    #     return False
+
+WRITABLE_POLITICS = [
+    TaskShareLevel.RW_ONLY_1_LEVELS,
+    TaskShareLevel.RW_ONLY_2_LEVELS,
+    TaskShareLevel.RW_ALL,
+]
+
+READABLE_POLITICS = [
+    TaskShareLevel.R_ONLY_1_LEVELS,
+    TaskShareLevel.R_ONLY_2_LEVELS,
+    TaskShareLevel.R_ALL,
+] + WRITABLE_POLITICS
 
 
 class Task(SqlAlchemyBase):
