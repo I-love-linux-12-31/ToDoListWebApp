44979c801ae28305c68cb39c9fa460b2068f788c
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-04-30 00:02:53 +0300

    Working on containerization.

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -2,4 +2,4 @@
 
 cp -r ./static/* /static/;
 
-uwsgi --http 0.0.0.0:5000 --wsgi-file app.py --callable app --master
+uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master
