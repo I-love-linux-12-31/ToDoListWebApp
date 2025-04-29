3dba0afce2f189266583640103d963a3c490ac3c
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-04-30 01:30:31 +0300

    Working on containerization.

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cp -r ./static/* /static/;
+mkdir -p /static/todo-app/static/ || true
+# cp -r ./static/* /static/;
+cp -r ./static/* /todo-app/static/ || true
+
 
 uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master
