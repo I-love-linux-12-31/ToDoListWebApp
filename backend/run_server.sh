19100baf22d17329e6ab1fac9ed1244fd3417bb6
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-05-01 16:32:21 +0300

    Working on a backend launch script

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -5,4 +5,4 @@ mkdir -p /static/todo-app/static/ || true
 cp -r ./static/* /todo-app/static/ || true
 
 
-uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master
+uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master --ini uWSGI.ini
