5ebedc7551cacf334d195a5bf80b0c82d9de083d
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-05-03 17:33:54 +0300

    Uploaded launcher script for better compatability.

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -3,6 +3,9 @@
 mkdir -p /static/todo-app/static/ || true
 # cp -r ./static/* /static/;
 cp -r ./static/* /todo-app/static/ || true
-
-
-uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master --ini uWSGI.ini
+if [ -f "/usr/bin/python3.14t" ]; then
+  uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master --ini uWSGI.ini
+else
+  # used default python3
+  uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master
+fi
