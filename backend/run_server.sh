345af77379b902767dc6a9e0d7feda966e74ef6a
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-05-03 19:15:29 +0300

    Fixed static files copying for nginx container on app startup

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -2,7 +2,8 @@
 
 mkdir -p /static/todo-app/static/ || true
 # cp -r ./static/* /static/;
-cp -r ./static/* /todo-app/static/ || true
+cp -r ./static/* /static/todo-app/static/ || true
+
 if [ -f "/usr/bin/python3.14t" ]; then
   uwsgi --plugin http --http 0.0.0.0:5000 --plugin python3 --wsgi-file app.py --callable app --master --ini uWSGI.ini
 else
