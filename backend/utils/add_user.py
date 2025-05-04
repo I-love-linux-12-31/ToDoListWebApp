5e34348656f145d16e0da05f32bf7f267775acd9
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-05-04 18:51:37 +0300

    Updated utils_cmd

--- backend/utils/add_user.py
+++ backend/utils/add_user.py
@@ -1,18 +1,17 @@
-from datetime import datetime
+from datetime import datetime, UTC
 
 from ORM.users import User
 from db import create_session
 
 from .common import ask_new_password, ask_boolean_question
 
-def add_user():
-    print(">> Add user")
-    username = input("Username: ")
-    email = input("Email: ")
-    created_at = datetime.now()
-    is_admin = ask_boolean_question("Is admin?")
-    password = ask_new_password()
 
+def add_user_not_interactive(username, password, email=None, is_admin=False, created_at=None, **kwargs):
+    # print("Adding user (not interactive)", username, password, email, is_admin, created_at)
+    if "admin" in kwargs and str(kwargs["admin"]).upper() in ("TRUE", "YES", "1"):
+        is_admin = True
+    if created_at is None:
+        created_at = datetime.now(UTC)
     try:
         with create_session() as session:
             user = User()
@@ -23,11 +22,22 @@ def add_user():
             user.password_hash = password
             session.add(user)
             session.commit()
+            return True
     except Exception as e:
         print("Failed to add user.")
         print(F"Error: {e.__class__.__name__}: {e}")
-        return
-    print("Added new user!")
+        return False
+
+
+def add_user():
+    print(">> Add user")
+    username = input("Username: ")
+    email = input("Email: ")
+    created_at = datetime.now()
+    is_admin = ask_boolean_question("Is admin?")
+    password = ask_new_password()
+    if add_user_not_interactive(username, password, email=email, is_admin=is_admin):
+        print("Added new user!")
 
 
 def main():
